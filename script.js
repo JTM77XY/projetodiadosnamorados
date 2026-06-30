3db178a6cf58bc8d4a0a112d66e1d6fe5377bea4
JTM77XY <jeantales773@gmail.com>
2026-06-30 17:04:58 -0300

    atualização

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-let imagens = document.getElementsByClassName('imagens')
+let imagens = document.querySelectorAll('.imagens')
 
-console.log(imagens);
 
-for(let i=0; i<imagens.length;i++){
-    imagens[i].addEventListener('click', function(){
-        alert("clicou na imagem" + imagens[i].src)
-    })
+imagens.forEach((img, index) => {
+            img.addEventListener('click', () => {
+                alert(`Você clicou na imagem ${index + 1}! Descrição: ${img.alt}`);
+            });
+        });
 
-}
+
+        
+
+// console.log(imagens);
+
+// for(let i=0; i<imagens.length;i++){
+//     imagens[i].addEventListener('click', function(){
+//         alert("clicou na imagem" + imagens[i].src)
+//     })
+
+// }
 
 
 
